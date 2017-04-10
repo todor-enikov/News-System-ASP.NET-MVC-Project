4ac3f4a324c60ea290eb3ce4aa75847466426b39
todor-enikov <enigmaenikov@gmail.com>
2017-04-10 17:19:22 +0300

    Added initial models for the database.

---
+++ Source/NewsSystem.Data.Models/Article.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace NewsSystem.Data.Models
+{
+    public class Article
+    {
+        [Key]
+        public Guid Id { get; set; }
+
+        public string Name { get; set; }
+
+        public string Description { get; set; }
+
+        public string ImagePath { get; set; }
+
+        public string UserId { get; set; }
+
+        public virtual User User { get; set; }
+    }
+}
---
+++ Source/NewsSystem.Data.Models/User.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.EntityFramework;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Security.Claims;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NewsSystem.Data.Models
+{
+    public class User : IdentityUser
+    {
+        private ICollection<Article> articles;
+
+        public User()
+        {
+            this.articles = new HashSet<Article>();
+        }
+
+        [Key]
+        public override string Id { get; set; }
+
+        public override string UserName { get; set; }
+
+        public string FirstName { get; set; }
+
+        public string LastName { get; set; }
+
+        //public virtual Guid ArticleId { get; set; }
+
+        public virtual ICollection<Article> Articles
+        {
+            get { return this.articles; }
+            set { this.articles = value; }
+        }
+
+        public ClaimsIdentity GenerateUserIdentity(UserManager<User> manager)
+        {
+            // Note the authenticationType must match the one defined in CookieAuthenticationOptions.AuthenticationType
+            var userIdentity = manager.CreateIdentity(this, DefaultAuthenticationTypes.ApplicationCookie);
+
+            // Add custom user claims here
+            return userIdentity;
+        }
+
+        public Task<ClaimsIdentity> GenerateUserIdentityAsync(UserManager<User> manager)
+        {
+            return Task.FromResult(GenerateUserIdentity(manager));
+        }
+    }
+}
