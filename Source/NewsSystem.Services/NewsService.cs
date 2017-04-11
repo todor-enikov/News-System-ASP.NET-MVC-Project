18b45bef2bf3b8ac1bff293ae573536a20a33aed
todor-enikov <enigmaenikov@gmail.com>
2017-04-11 17:16:21 +0300

    Re-adding...

--- Source/NewsSystem.Services/NewsService.cs
+++ Source/NewsSystem.Services/NewsService.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace NewsSystem.Services
 {
-    class NewsService
+    public class NewsService
     {
     }
 }
--- Source/NewsSystem.Services/UserService.cs
+++ Source/NewsSystem.Services/UserService.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace NewsSystem.Services
 {
-    public class Class1
+    public class UserService
     {
     }
 }
