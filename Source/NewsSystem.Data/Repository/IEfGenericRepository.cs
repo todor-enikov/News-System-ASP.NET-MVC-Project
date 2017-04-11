60668f7d48240a7e36d4cdd10ce06c04641c7cb6
todor-enikov <enigmaenikov@gmail.com>
2017-04-11 11:31:36 +0300

    Added repository in the project.

---
+++ Source/NewsSystem.Data/Repository/IEfGenericRepository.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Newssystem.Data.Repository
+{
+    public interface IEfGenericRepository<T>
+          where T : class
+    {
+        IEnumerable<T> All();
+
+        T GetById(object id);
+
+        void Add(T entity);
+
+        void Update(T entity);
+
+        void Delete(T entity);
+
+        void Delete(object id);
+
+        int SaveChanges();
+    }
+}
