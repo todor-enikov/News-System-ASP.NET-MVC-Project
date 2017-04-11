96867f44fb266ce03b575cd4d80e0b7443dfb19d
todor-enikov <enigmaenikov@gmail.com>
2017-04-11 19:46:00 +0300

    Implemented basic methods for NewsService.

--- Source/NewsSystem.Services/NewsService.cs
+++ Source/NewsSystem.Services/NewsService.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using Newssystem.Data.Repository;
+using NewsSystem.Data.Models;
+using NewsSystem.Services.Contracts;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +9,45 @@ using System.Threading.Tasks;
 
 namespace NewsSystem.Services
 {
-    public class NewsService
+    public class NewsService : INewsService
     {
+        private readonly IEfGenericRepository<Article> newsRepo;
+
+        public NewsService(IEfGenericRepository<Article> newsRepo)
+        {
+            if (newsRepo == null)
+            {
+                throw new ArgumentException("The car repo should not be null!");
+            }
+
+            this.newsRepo = newsRepo;
+        }
+
+        public IQueryable<Article> GetAllNews()
+        {
+            return this.newsRepo.All();
+        }
+
+        public Article GetNewsById(string id)
+        {
+            return this.newsRepo.GetById(id);
+        }
+
+        public void AddNews(Article newsToAdd)
+        {
+            this.newsRepo.Add(newsToAdd);
+            this.newsRepo.SaveChanges();
+        }
+
+        public void UpdateNews(Article newsToUpdate)
+        {
+            this.newsRepo.Update(newsToUpdate);
+            this.newsRepo.SaveChanges();
+        }
+
+        public void DeleteNews(Article newsToDelete)
+        {
+            this.newsRepo.Delete(newsToDelete);
+        }
     }
 }
