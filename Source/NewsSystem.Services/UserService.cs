df0107b5bb8c917b423f3ed5d82e5001c9e96aae
todor-enikov <enigmaenikov@gmail.com>
2017-04-11 19:46:18 +0300

    Implemented basic methods for UserService.

--- Source/NewsSystem.Services/UserService.cs
+++ Source/NewsSystem.Services/UserService.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using Newssystem.Data.Repository;
+using NewsSystem.Data.Models;
+using NewsSystem.Services.Contracts;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +9,34 @@ using System.Threading.Tasks;
 
 namespace NewsSystem.Services
 {
-    public class UserService
+    public class UserService : IUserService
     {
+        private readonly IEfGenericRepository<User> userRepo;
+
+        public UserService(IEfGenericRepository<User> userRepo)
+        {
+            if (userRepo == null)
+            {
+                throw new ArgumentException("The car repo should not be null!");
+            }
+
+            this.userRepo = userRepo;
+        }
+
+        public IQueryable<User> GetAllUsers()
+        {
+            return this.userRepo.All();
+        }
+
+        public User GetUserById(string id)
+        {
+            return this.userRepo.GetById(id);
+        }
+
+        public void UpdateUserRole(User userToUpdate)
+        {
+            this.userRepo.Update(userToUpdate);
+            this.userRepo.SaveChanges();
+        }
     }
 }
