f44e3722de45e10aee9f30ffda4e347e74468f29
todor-enikov <enigmaenikov@gmail.com>
2017-04-11 17:15:37 +0300

    Added classes for the main services of the application.

---
+++ Source/NewsSystem.Services/UploadImageService.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NewsSystem.Services
+{
+    public class UploadImageService
+    {
+        public string ImagePath()
+        {
+            // To Do: Implement uploading image and returning the path of the image.
+            return "";
+        }
+    }
+}
