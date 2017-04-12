638af828903b51ac1e6091392ee2d0992b91c5eb
todor-enikov <enigmaenikov@gmail.com>
2017-04-12 20:22:58 +0300

    Moved Auth logic in separate library.

---
+++ Source/NewsSystem.Auth/SignInManager.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNet.Identity.Owin;
+using Microsoft.Owin;
+using Microsoft.Owin.Security;
+using NewsSystem.Data.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NewsSystem.Auth
+{
+    public class SignInManager : SignInManager<User, string>
+    {
+        public SignInManager(UserManager userManager, IAuthenticationManager authenticationManager)
+            : base(userManager, authenticationManager)
+        {
+        }
+
+        public override Task<ClaimsIdentity> CreateUserIdentityAsync(User user)
+        {
+            return user.GenerateUserIdentityAsync((UserManager)UserManager);
+        }
+
+        public static SignInManager Create(IdentityFactoryOptions<SignInManager> options, IOwinContext context)
+        {
+            return new SignInManager(context.GetUserManager<UserManager>(), context.Authentication);
+        }
+    }
+}
