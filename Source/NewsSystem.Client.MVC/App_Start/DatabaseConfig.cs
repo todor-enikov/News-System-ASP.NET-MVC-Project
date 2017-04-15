be9fe568b25c746a3d36d5f8274e01c72d1d46ad
todor-enikov <enigmaenikov@gmail.com>
2017-04-15 18:10:25 +0300

    Added initial MVC client, with setup it connection string to the database and successfully registering and logining user.

---
+++ Source/NewsSystem.Client.MVC/App_Start/DatabaseConfig.cs
@@ -0,0 +1,19 @@
+﻿using Newssystem.Data;
+using Newssystem.Data.Migrations;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace NewsSystem.Client.MVC.App_Start
+{
+    public class DatabaseConfig
+    {
+        public static void Initialize()
+        {
+            Database.SetInitializer(new MigrateDatabaseToLatestVersion<NewsSystemDbContext, Configuration>());
+            NewsSystemDbContext.Create().Database.Initialize(true);
+        }
+    }
+}
---
+++ Source/NewsSystem.Client.MVC/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="NewsSystem.Client.MVC.MvcApplication" Language="C#" %>
