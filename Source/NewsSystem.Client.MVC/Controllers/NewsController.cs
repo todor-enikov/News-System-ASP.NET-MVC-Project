671a4e4faccaeb41e68a8bfcc69b77e249323927
todor-enikov <enigmaenikov@gmail.com>
2017-04-16 20:46:28 +0300

    Created initial controllers and pages for them, NewsController and UserController.

---
+++ Source/NewsSystem.Client.MVC/Controllers/NewsController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace NewsSystem.Client.MVC.Controllers
+{
+    public class NewsController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult AddNews()
+        {
+            return View();
+        }
+    }
+}
