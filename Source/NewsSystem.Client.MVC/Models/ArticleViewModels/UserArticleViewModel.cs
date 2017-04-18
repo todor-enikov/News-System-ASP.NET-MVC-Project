27a8e502fef85b307dc51de734e5f0cde9f5b994
todor-enikov <enigmaenikov@gmail.com>
2017-04-18 13:32:36 +0300

    Added view models for user details and articles of the user.

---
+++ Source/NewsSystem.Client.MVC/Models/ArticleViewModels/UserArticleViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace NewsSystem.Client.MVC.Models.ArticleViewModels
+{
+    public class UserArticleViewModel
+    {
+        public Guid Id { get; set; }
+
+        public string Title { get; set; }
+    }
+}
---
+++ Source/NewsSystem.Client.MVC/Models/UserViewModels/UserDetailsViewModel.cs
@@ -0,0 +1,21 @@
+﻿using NewsSystem.Client.MVC.Models.ArticleViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace NewsSystem.Client.MVC.Models.UserViewModels
+{
+    public class UserDetailsViewModel
+    {
+        public string FirstName { get; set; }
+
+        public string LastName { get; set; }
+
+        public string UserName { get; set; }
+
+        public string Email { get; set; }
+
+        public List<UserArticleViewModel> UserArticles { get; set; }
+    }
+}
