81f7a4fc4544216601de52be83fc14510d84c4bd
todor-enikov <enigmaenikov@gmail.com>
2017-04-18 12:51:12 +0300

    Added AllUsersViewModel and initial Details view.

---
+++ Source/NewsSystem.Client.MVC/Models/UserViewModels/AllUsersViewModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace NewsSystem.Client.MVC.Models.UserViewModels
+{
+    public class AllUsersViewModel
+    {
+        public string Id { get; set; }
+
+        public string Username { get; set; }
+            
+        public string FirstName { get; set; }
+
+        public string LastName { get; set; }
+    }
+}
