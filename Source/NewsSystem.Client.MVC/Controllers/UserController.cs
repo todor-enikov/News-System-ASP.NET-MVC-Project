c0d68414f6feb0ad526f950c8286ed4d46fe9177
todor-enikov <enigmaenikov@gmail.com>
2017-04-18 12:53:02 +0300

    Implemented logic for listing all users in Index.cshtml and used the new views and methods (from UserService).

--- Source/NewsSystem.Client.MVC/Controllers/UserController.cs
+++ Source/NewsSystem.Client.MVC/Controllers/UserController.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using NewsSystem.Client.MVC.Models.UserViewModels;
+using NewsSystem.Services.Contracts;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -8,9 +10,70 @@ namespace NewsSystem.Client.MVC.Controllers
 {
     public class UserController : Controller
     {
+        private readonly IUserService userService;
+
+        public UserController(IUserService userService)
+        {
+            this.userService = userService;
+        }
+
+        [HttpGet]
         public ActionResult Index()
         {
+            var allUsersFromDb = this.userService.GetAllUsers();
+            var viewModel = new List<AllUsersViewModel>();
+
+            foreach (var user in allUsersFromDb)
+            {
+                var userToAdd = new AllUsersViewModel()
+                {
+                    Id = user.Id,
+                    Username = user.UserName,
+                    FirstName = user.FirstName,
+                    LastName = user.LastName
+                };
+
+                viewModel.Add(userToAdd);
+            }
+
+            return View(viewModel);
+        }
+
+        [HttpGet]
+        public ActionResult Details()
+        {
             return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Search(string search)
+        {
+
+            var userModel = this.userService
+                                .GetUserByUserName(search);
+
+            var viewModel = new List<AllUsersViewModel>();
+
+            foreach (var user in userModel)
+            {
+                var currentUser = new AllUsersViewModel()
+                {
+                    Id = user.Id,
+                    Username = user.UserName,
+                    FirstName = user.FirstName,
+                    LastName = user.LastName
+                };
+
+                viewModel.Add(currentUser);
+            }
+
+            if (viewModel.Count < 1)
+            {
+                return PartialView("_NoResults", viewModel);
+            }
+
+            return PartialView("_AllUsers", viewModel);
         }
     }
 }
