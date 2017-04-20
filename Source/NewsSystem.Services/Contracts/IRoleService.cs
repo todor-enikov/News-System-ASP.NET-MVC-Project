8e38d28cb445583b482e4871d756f62d78fb0102
todor-enikov <enigmaenikov@gmail.com>
2017-04-20 22:42:00 +0300

    Created RoleService and IRoleService for returning the role of specific user.

---
+++ Source/NewsSystem.Services/Contracts/IRoleService.cs
@@ -0,0 +1,14 @@
+﻿using NewsSystem.Data.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NewsSystem.Services.Contracts
+{
+    public interface IRoleService
+    {
+        string GetCurrentRoleOfUser(string id);
+    }
+}
---
+++ Source/NewsSystem.Services/RoleService.cs
@@ -0,0 +1,32 @@
+﻿using Newssystem.Data.Repository;
+using NewsSystem.Services.Contracts;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data;
+using NewsSystem.Data.Models;
+using Newssystem.Data;
+using System.Web.Security;
+
+namespace NewsSystem.Services
+{
+    public class RoleService : IRoleService
+    {
+        private readonly NewsSystemDbContext dbContext;
+
+        public RoleService()
+        {
+            this.dbContext = new NewsSystemDbContext();
+        }
+
+        public string GetCurrentRoleOfUser(string id)
+        {
+            return dbContext.Roles
+                                  .Where(x => x.Id == id)
+                                  .FirstOrDefault()
+                                  .Name;
+        }
+    }
+}
