1aaf9d59f14f8d17eea097a9b2661c6dbc7ca054
todor-enikov <enigmaenikov@gmail.com>
2017-04-21 18:26:11 +0300

    Created NewsDetailsViewModel and view Edit.

---
+++ Source/NewsSystem.Client.MVC/Models/NewsViewModels/NewsDetailsViewModel.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace NewsSystem.Client.MVC.Models.NewsViewModels
+{
+    public class NewsDetailsViewModel
+    {
+        public Guid Id { get; set; }
+
+        public string Title { get; set; }
+
+        public string Resume { get; set; }
+
+        public string Content { get; set; }
+
+        public string ImagePath { get; set; }
+
+        public DateTime AddedOn { get; set; }
+
+        public string AuthorId { get; set; }
+
+        public string Author { get; set; }
+    }
+}
