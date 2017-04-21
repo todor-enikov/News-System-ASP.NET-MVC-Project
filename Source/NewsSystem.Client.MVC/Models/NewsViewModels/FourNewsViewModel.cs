a6f06ba8c8bb8297d5e9464ea895a736d3e97057
todor-enikov <enigmaenikov@gmail.com>
2017-04-21 20:39:31 +0300

    Created FourNewsViewModel.

---
+++ Source/NewsSystem.Client.MVC/Models/NewsViewModels/FourNewsViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace NewsSystem.Client.MVC.Models.NewsViewModels
+{
+    public class FourNewsViewModel
+    {
+        public Guid Id { get; set; }
+
+        public string Title { get; set; }
+
+        public string ImagePath { get; set; }
+    }
+}
