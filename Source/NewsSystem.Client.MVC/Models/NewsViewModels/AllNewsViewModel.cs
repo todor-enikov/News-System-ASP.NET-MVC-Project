ab5ef4bede62ea2ba98586daa130907243c71214
todor-enikov <enigmaenikov@gmail.com>
2017-04-21 15:30:25 +0300

    Created partial view _AllNews.cshtml and AllNewsViewModel.

---
+++ Source/NewsSystem.Client.MVC/Models/NewsViewModels/AllNewsViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace NewsSystem.Client.MVC.Models.NewsViewModels
+{
+    public class AllNewsViewModel
+    {
+        public Guid Id { get; set; }
+
+        public string Title { get; set; }
+    }
+}
